44d632b494505a29c2b44b8ef0113a322329f126
CPF32 <chrisfarish32@gmail>
2020-11-06 16:44:39 -0600

    D2 End

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,16 +5,17 @@ $(() => {
   $('input[type="submit"]').mousedown(() => {
     $(this).css('background', '#2ecc71')
   })
-
   $('input[type="submit"]').mouseup(() => {
     $(this).css('background', '#1abc9c')
   })
-
   $('#loginform').click(() => {
     $('.login').fadeToggle('slow')
     $(this).toggleClass('green')
   })
-
+  $('#registerform').click(() => {
+    $('.register').fadeToggle('slow')
+    $(this).toggleClass('green')
+  })
   $(document).mouseup((e) => {
     const container = $('.login')
 
@@ -23,4 +24,15 @@ $(() => {
       $('#loginform').removeClass('green')
     }
   })
+  $(document).mouseup((e) => {
+    const container = $('.register')
+
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      container.hide()
+      $('#registerform').removeClass('green')
+    }
+  })
+
+  $('#registerform').on('submit', authEvents.signUpEvent)
+  $('#loginform').on('submit', authEvents.signInEvent)
 })
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,38 +3,40 @@ const store = require('./../store')
 
 // SIGN-UP HANDLERS
 const signUpSuccess = (response) => {
-
+  console.log('Sign-Up: ' + response.user.email)
 }
 
 const signUpFailure = () => {
-
+  console.log('Sign-Up Failed')
 }
 
 // SIGN-IN HANDLERS
 const signInSuccess = (response) => {
   store.user = response.user
+  console.log('Sign-In: ' + store.user)
 }
 
 const signInFailure = () => {
-
+  console.log('Sign-In Failed')
 }
 
 // SIGN-OUT HANDLERS
 const signOutSuccess = () => {
   store.user = null
+  console.log('Sign-Out')
 }
 
 const signOutFailure = () => {
-
+  console.log('Sign-Out Failed')
 }
 
 // CHANGE-PASSWORD HANDLERS
 const changePasswordSuccess = () => {
-
+  console.log('Password-Change: ' + store.user)
 }
 
 const changePasswordFailure = () => {
-
+  console.log('Password-Change Failed')
 }
 
 module.exports = {
