b88c477572198605b2120d7f5c724415d0fdaed8
CPF32 <chrisfarish32@gmail>
2020-11-10 10:43:38 -0600

    formfield for app

---
+++ assets/scripts/app/ui.js
@@ -0,0 +1,46 @@
+'use strict'
+const store = require('./../store')
+
+// ADD-RUN HANDLERs
+const addRunSuccess = (response) => {
+
+}
+
+const addRunFailure = () => {
+
+}
+
+const deleteRunSuccess = (response) => {
+
+}
+
+const deleteRunFailure = () => {
+
+}
+
+const editRunSuccess = (response) => {
+
+}
+
+const editRunFailure = () => {
+
+}
+
+const indexRunSuccess = (response) => {
+
+}
+
+const indexRunFailure = () => {
+
+}
+
+module.exports = {
+  addRunSuccess,
+  addRunFailure,
+  deleteRunSuccess,
+  deleteRunFailure,
+  editRunSuccess,
+  editRunFailure,
+  indexRunSuccess,
+  indexRunFailure
+}
